7f5f0cf8fdcd73d9e4e1a0e8cc4474eabbb0da60
Georgi254 <georgi.gyovchev254@gmail.com>
2023-01-20 20:51:30 +0000

    Trabajo hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+document.querySelector(".card").classList.add(RandomSuit());
+document.querySelector(".card").innerHTML = RandomNumber(); 
+
 };
+
+let suit = ['heart', 'spade', 'club', 'diamond']
+let number = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+let suitIndex = [Math.floor(Math.random() * suit.length)]
+let numberIndex = [Math.floor(Math.random() * number.length)]
+function RandomSuit(){
+return suit[suitIndex]
+}
+
+function RandomNumber(){
+  return number[numberIndex]
+}
+
+
